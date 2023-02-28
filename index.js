3efc3d2aeeb837d1bd545672ffd4aaa01131d4b0
RitenTheGoat <125036599+RitenTheGoat@users.noreply.github.com>
2023-02-28 15:44:05 +0100

    Serverapp
    Server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get("/", function(req,res){
+
+    res.send("ok")
+
+
+
+})
+app.get('/:imie/:nazwisko', function(req,res){
+    const imie= req.params.imie
+    const nazwisko = req.params.nazwisko
+    res.send(imie)
+    console.log(imie)
+    console.log(nazwisko)
+
+})
+
+
+app.listen(3000)
